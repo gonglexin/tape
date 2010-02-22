2aa493de31b704a7fe6cec07d7bbad204db65113
thiefhandsome <gonglexin@gmail.com>
2010-02-22 21:56:09 +0800

    add some validation

--- app/models/tape.rb
+++ app/models/tape.rb
@@ -1,2 +1,4 @@
 class Tape < ActiveRecord::Base
+  validates_presence_of :title, :image_url
+  validates_format_of :image_url, :with => /\.(gif|jpg|png)$/i, :message => "must be a URL for GIF, JPG or PNG image."
 end
