68866808329ed107ca8858dbdd0a6a3a30ca832a
thiefhandsome <gonglexin@gmail.com>
2010-02-23 23:21:37 +0800

    login filter and something else

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,20 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  #Todo 过滤掉正确的地址
+  before_filter :authorize, :except => :login
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  protected
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        session[:original_uri] = request.request_uri
+        flash[:notice] = "Please login"
+        redirect_to :controller => 'admin', :action => 'login'
+      end
+    end
 end
