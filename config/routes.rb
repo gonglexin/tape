bec606a55d617bf186228357b4f71cf43122211a
thiefhandsome <gonglexin@gmail.com>
2010-02-23 16:31:55 +0800

    remove resouces of tapes and contacts in the front client

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :tapes
+  # map.resources :tapes
 
-  map.resources :contacts
+  # map.resources :contacts
 
   # The priority is based upon order of creation: first created -> highest priority.
 
