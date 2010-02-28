0789962a8c608f66cdb6bbf2485fe755d1b477b9
thiefhandsome <gonglexin@gmail.com>
2010-02-28 22:56:34 +0800

    what

--- app/models/contact.rb
+++
@@ -1,6 +0,0 @@
-class Contact < ActiveRecord::Base
-  EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates_presence_of :name, :email, :tel, :fax, :address
-  validates_format_of :email, :with => EmailRegex
-  validates_numericality_of :tel, :fax
-end
